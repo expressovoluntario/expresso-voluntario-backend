b3c48cc348eaf1431fd5124161eaec03a794d482
Willian Ribeiro <willianribeiro.contato@gmail.com>
2016-06-13 17:51:37 -0300

    Save tags and add search by location

--- task/resources.py
+++ task/resources.py
@@ -18,15 +18,17 @@ class TaskResource(Resource):
         parser.add_argument('title', required=True, type=str)
         parser.add_argument('description', required=True, type=str)
         parser.add_argument('status', type=str)
+        parser.add_argument('tags', type=list, location='json')
         parser.add_argument('ong_id', required=True, type=str)
         args = parser.parse_args(strict=True)
 
         title = args.get('title')
         description = args.get("description")
         status = args.get("status")
+        tags = args.get("tags")
         ong_id = args.get('ong_id')
 
-        task = TaskDocument(title=title, description=description, status=status).save()
+        task = TaskDocument(title=title, description=description, status=status, tags=tags).save()
 
         ong = OngDocument.objects.get_or_404(id=ong_id)
         ong.tasks.append(task)
@@ -44,19 +46,32 @@ class TaskResource(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('limit', type=int)
         parser.add_argument('tag', type=str)
+        parser.add_argument('location', type=str)
         args = parser.parse_args(strict=True)
         limit = args.get("limit", None)
         tag = args.get('tag', None)
+        location = args.get('location', None)
 
-        if not any([id_, limit, tag]):
+        if not any([id_, limit, tag, location]):
             abort(400, message="You must provide an id, limit or tag")
 
         elif limit is not None:
             tasks = TaskDocument.objects[:limit]
             return [task.to_dict() for task in tasks], 200
+
         elif tag is not None:
             tasks = TaskDocument.objects(tags__in=[tag])
             return [task.to_dict() for task in tasks], 200
+
+        elif location is not None:
+            ongs = OngDocument.objects(address__localidade__in=[location]).all()
+
+            tasks = []
+            for ong in ongs:
+                tasks.extend(ong.tasks)
+
+            return [task.to_dict() for task in tasks], 200
+
         task = TaskDocument.objects.get_or_404(id=id_)
         return task.to_dict(), 200
 
